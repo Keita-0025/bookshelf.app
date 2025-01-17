9aa5302188fcf7a665c306d02a1de5201070e646
Keita-0025 <ku74n1@gmail.com>
2025-01-17 22:51:51 +0900

    fix: Update MongoDB connection options for Mongoose
    - Replace serverApi options with useNewUrlParser and useUnifiedTopology
    - Ensure compatibility with Mongoose and improve connection handling

--- server/helpers/db.mjs
+++ server/helpers/db.mjs
@@ -1,13 +1,10 @@
-import { connect } from 'mongoose';
+import mongoose,{ connect } from 'mongoose';
 import env from 'dotenv'
 
 env.config();
 
 connect(process.env.MONGO_URI, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
