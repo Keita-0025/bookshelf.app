5e07ddbcb935f10340fe85ff7979d0b01c6d2950
Keita-0025 <ku74n1@gmail.com>
2025-01-17 21:28:20 +0900

    feat: Define Book model with bookSchema
    - Create 'server/models' directory
    - Define bookSchema and create Book model

---
+++ server/models/book.mjs
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose'
+
+const bookSchema = new Schema({
+  title: { 
+    type: String,
+     required: true
+     },
+  description: {
+     type: String, 
+     required: true 
+    },
+  rating: {
+      type: Number,
+      enum: [1, 2, 3, 4, 5],
+      required: true,
+      get: val => Math.round(val),
+      set: val => Math.round(val),
+    },
+  comment: {
+     type: String,
+      required: true 
+    },
+}, { timestamps: true })
+
+
+export default Book = model('Book', bookSchema);
+
+
